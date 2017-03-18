8ced8a2df5cd189128fae6fda97b4b9d3dc089b5
Danica Rovó <hunny154@gmail.com>
2017-03-18 18:35:48 +0100

    added and edited game engine script
    doesn't work:(

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	public Text question;
+	public Text instructions;
+	public int guess;
+	int max;
+	int min;
+	//string scene;
+	
+	void Start () {
+	//StartGame();
+		instructions.text = "[Press the up arrow or the button if your number is higher, down or button if lower, enter if I got it right!]";
+		min = 1;
+		max = 1000;
+		guess = 500;
+	}
+	
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.DownArrow)) {
+	Lower ();
+	}
+	if(Input.GetKeyDown(KeyCode.UpArrow)){
+	Higher ();
+	}
+	/*if(Input.GetKeyDown (KeyCode.Return)) {
+	Win (scene);
+	}*/
+	}
+	
+	/*void StartGame(){
+	instructions.text = "[Press the up arrow or the button if your number is higher, down or button if lower, enter if I got it right!]";
+	min = 1;
+	max = 1000;
+	guess = 500;
+	}*/
+	
+	public void Higher(){
+	min = guess;
+	guess = (max+min) / 2;
+	question.text = "Is your number high?";
+	}
+	
+	public void Lower(){
+	max = guess;
+	guess = (max+min) / 2;
+	question.text = "Is your number";
+	}
+	
+	/*public void Win(string scene){
+	Application.LoadLevel (scene);
+	}*/
+}
+
